4aa2e4c54581c66f69a71b0ea82b8f1172a5e592
takamitsu kobayashi <kobayashi@crss-m.jp>
2022-02-19 19:34:03 +0900

    予約変更submit時のconfirmをキャンセルしたときに二重送信classが解除されるように修正

--- resources/views/staff/common/_check_return_date_js.blade.php
+++ resources/views/staff/common/_check_return_date_js.blade.php
@@ -5,7 +5,12 @@ $(function(){
       const dt = new Date();
       const returnDate = new Date(`${rd} 23:59:59`);
       if(dt.getTime() > returnDate.getTime()){ // 帰着日が過去
-        return confirm("帰着日が過去の日付で登録すると催行済に移動します。\nよろしいですか?");
+        if(confirm("帰着日が過去の日付で登録すると催行済に移動します。\nよろしいですか?")){
+          return true;
+        }else{
+          $("form .doubleBan").css("pointer-events", "auto"); // 二重送信防止class除去
+          return false;
+        }
       }
     }
     return true;
