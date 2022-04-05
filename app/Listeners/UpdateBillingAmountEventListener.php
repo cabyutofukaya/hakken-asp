d5ed1fe848d2a2837d6a441291b248e9deef9ecc
takamitsu kobayashi <kobayashi@crss-m.jp>
2022-04-05 18:31:45 +0900

    行程編集時に料金関連カラム情報を請求書、見積/予約確認書に保存するように修正。料金カラム情報が含まれていないと法人向け一括請求書の合計金額計算が最新情報で更新できないため。見積/予約確認書は一括請求と関係はないが処理を合わせるために実装

--- app/Listeners/UpdateBillingAmountEventListener.php
+++ app/Listeners/UpdateBillingAmountEventListener.php
@@ -52,15 +52,34 @@ class UpdateBillingAmountEventListener
         }
 
         if ($reserveInvoice) { // 請求書データがある場合のみ処理
-            // 行程データからオプション価格情報、航空券価格情報、ホテル価格情報を取得
-            list($optionPrices, $airticketPrices, $hotelPrices, $disp1, $disp2) = $this->getPriceAndHotelInfo($reserveItinerary, true);
+            // 行程データからオプション価格情報、航空券価格情報、ホテル価格情報等を取得
+            list($optionPrices, $airticketPrices, $hotelPrices, $hotelInfo, $hotelContacts) = $this->getPriceAndHotelInfo($reserveItinerary, false);
 
-            // 書類に設定された参加者情報から合計金額を計算
-            $amountTotal = get_price_total($reserveInvoice->participant_ids, $optionPrices, $airticketPrices, $hotelPrices);
+            // // 書類に設定された参加者情報から合計金額を計算
+            // $amountTotal = get_price_total($reserveInvoice->participant_ids, $optionPrices, $airticketPrices, $hotelPrices);
 
             // 合計金額が変わっていれば請求書、再計算処理を実行
-            if ($amountTotal !== $reserveInvoice->amount_total) {
-                $ri = $this->reserveInvoiceService->updateOrCreate(['reserve_itinerary_id' => $reserveItinerary->id], ['amount_total' => $amountTotal]);
+            // if ($amountTotal !== $reserveInvoice->amount_total) {
+
+            // 料金に関する設定が変わっていたら再計算(hotel_infoとhotel_contactsは念の為判定材料に)
+            if ($reserveInvoice->option_prices != $optionPrices ||
+                $reserveInvoice->airticket_prices != $airticketPrices ||
+                $reserveInvoice->hotel_prices != $hotelPrices ||
+                $reserveInvoice->hotel_info != $hotelInfo ||
+                $reserveInvoice->hotel_contacts != $hotelContacts
+            ) {
+                // 書類に設定された参加者情報から合計金額を計算
+                $amountTotal = get_price_total($reserveInvoice->participant_ids, $optionPrices, $airticketPrices, $hotelPrices);
+
+                $ri = $this->reserveInvoiceService->updateOrCreate(['reserve_itinerary_id' => $reserveItinerary->id], [
+                    'option_prices' => $optionPrices,
+                    'airticket_prices' => $airticketPrices,
+                    'hotel_prices' => $hotelPrices,
+                    'hotel_info' => $hotelInfo,
+                    'hotel_contacts' => $hotelContacts,
+                    // 合計金額
+                    'amount_total' => $amountTotal
+                ]);
 
                 // 入金額の再計算＆更新した最新の請求情報を取得
                 $newReserveInvoice = $this->reserveInvoiceService->updateDepositAmount($ri);
@@ -87,14 +106,35 @@ class UpdateBillingAmountEventListener
         if ($reserveConfirms) {
             foreach ($reserveConfirms as $reserveConfirm) {
                 // 行程データからオプション価格情報、航空券価格情報、ホテル価格情報を取得
-                list($optionPrices, $airticketPrices, $hotelPrices, $disp1, $disp2) = $this->getPriceAndHotelInfo($reserveItinerary, true);
+                list($optionPrices, $airticketPrices, $hotelPrices, $hotelInfo, $hotelContacts) = $this->getPriceAndHotelInfo($reserveItinerary, false);
     
-                // 書類に設定された参加者情報から合計金額を計算
-                $amountTotal = get_price_total($reserveConfirm->participant_ids, $optionPrices, $airticketPrices, $hotelPrices);
+                // // 書類に設定された参加者情報から合計金額を計算
+                // $amountTotal = get_price_total($reserveConfirm->participant_ids, $optionPrices, $airticketPrices, $hotelPrices);
 
-                // 合計金額が変わっていれamount_totalカラムを更新
-                if ($amountTotal !== $reserveConfirm->amount_total) {
-                    $this->reserveConfirmService->updateAmountTotal($reserveConfirm->id, $amountTotal);
+                // // 合計金額が変わっていれamount_totalカラムを更新
+                // if ($amountTotal !== $reserveConfirm->amount_total) {
+
+                // 料金に関する設定が変わっていたら再計算(hotel_infoとhotel_contactsは念の為判定材料に)
+                if ($reserveConfirm->option_prices != $optionPrices ||
+                    $reserveConfirm->airticket_prices != $airticketPrices ||
+                    $reserveConfirm->hotel_prices != $hotelPrices ||
+                    $reserveConfirm->hotel_info != $hotelInfo ||
+                    $reserveConfirm->hotel_contacts != $hotelContacts
+                ) {
+                    // 書類に設定された参加者情報から合計金額を計算
+                    $amountTotal = get_price_total($reserveConfirm->participant_ids, $optionPrices, $airticketPrices, $hotelPrices);
+
+                    // $this->reserveConfirmService->updateAmountTotal($reserveConfirm->id, $amountTotal);
+
+                    $this->reserveConfirmService->updateOrCreate(['id' => $reserveConfirm->id], [
+                        'option_prices' => $optionPrices,
+                        'airticket_prices' => $airticketPrices,
+                        'hotel_prices' => $hotelPrices,
+                        'hotel_info' => $hotelInfo,
+                        'hotel_contacts' => $hotelContacts,
+                        // 合計金額
+                        'amount_total' => $amountTotal
+                    ]);
                 }
             }
         }
