16190cd3665da00b1d0a06c9b5802056ce3a3c7d
takamitsu kobayashi <kobayashi@crss-m.jp>
2022-05-22 01:04:07 +0900

    未払合計計算時、account_payable_detailsレコードのunpaid_balanceが1円以上の値を対象とするように修正。マイナス金額が合計されると相殺されてしまい適切な金額が計算できなくなるため

--- app/Models/Reserve.php
+++ app/Models/Reserve.php
@@ -795,9 +795,7 @@ class Reserve extends Model
     {
         // 計算は予約ステータス時のみ
         if ($this->application_step == config('consts.reserves.APPLICATION_STEP_RESERVE')) {
-            // ↓有効な仕入のみ(enabled_account_payable_details)にすると取り消しユーザーで支払い済みの仕入があった場合に計算対象外になってしまうため、enabled_account_payable_detailsに変更
-            // return $this->enabled_account_payable_details->sum('unpaid_balance');
-            return $this->account_payable_details->sum('unpaid_balance');
+            return $this->account_payable_details->where('unpaid_balance', '>', 0)->sum('unpaid_balance');
         } else {
             return 0;
         }
